b26e3baa73ad5ac897064884e143cd3b074e66cd
Ananya Pathak <anyalko043@gmail.com>
2025-09-05 03:29:16 +0530

    feat : add simple proof-of-work block mining

--- index.js
+++ index.js
@@ -66,8 +66,66 @@ function findingHashwithPrevHash(block, prefix){
 }
 
 // core concept : block structure with transactions, nonce, and prevHash
-const result = findingHashwithPrevHash(block, "dev")
-console.log(`Result: ${result.nonce}`);
-console.log(`input: ${result.hash}`);
+// const result = findingHashwithPrevHash(block, "dev")
+// console.log(`Result: ${result.nonce}`);
+// console.log(`input: ${result.hash}`);
 
 
+// Assignment 4 How can I link multiple blocks into a chain, where each block uses the previous block’s hash?
+
+// this is how thinks work under the hood (conceptually)
+function calculateHash(block){
+  return crypto.createHash('sha256')
+  .update(JSON.stringify(block.transactions)+ block.nonce + block.prevHash)
+  .digest("hex")
+}
+
+//mine block --> simple means just find hash starting with '000'
+function mineBlock(transactions, prevHash){
+  let nonce = 0;
+  while(true){
+    const block = { transactions, nonce,  prevHash};
+    const hash = calculateHash(block);
+    if(hash.startsWith("000")){
+      return {...block, hash};
+    }
+    nonce++;
+  }
+}
+
+//Genesis block
+const genesisBlock = mineBlock(["Genesis block"], "0");
+
+// Next block lines to genesis
+const block2 = mineBlock(["userNameA => userNameB | Rs 50"], genesisBlock.hash);
+
+// Next block links to block2
+const block3 = mineBlock(["userNameC => userNameD | Rs 10"], block2.hash);
+
+console.log(genesisBlock);
+console.log(block2);
+console.log(block3);
+
+
+//output 
+/*
+node index.js
+{
+  transactions: [ 'Genesis block' ],
+  nonce: 1359,
+  prevHash: '0',
+  hash: '000c5ad35292007f7c33e6890a07e5b49e62bf8fb9379437c8b4f9e36ab9e2b6'
+}
+{
+  transactions: [ 'userNameA => userNameB | Rs 50' ],
+  nonce: 1744,
+  prevHash: '000c5ad35292007f7c33e6890a07e5b49e62bf8fb9379437c8b4f9e36ab9e2b6',
+  hash: '000f68c3705edf6eed9eb07a8a63f3e9ba516ce217c113ddfbd971b9c7930346'
+}
+{
+  transactions: [ 'userNameC => userNameD | Rs 10' ],
+  nonce: 516,
+  prevHash: '000f68c3705edf6eed9eb07a8a63f3e9ba516ce217c113ddfbd971b9c7930346',
+  hash: '000f7c10c0eacc7b247aaae2541f602a850b29e333cf75777e100755f82dbc1a'
+}
+ */
